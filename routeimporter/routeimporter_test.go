f434d94b27a3c7752016aa01b3cdc8f85ce8d106
Prasenjit Adhikary <prasenjit.adhikary@keysight.com>
2023-08-24 17:08:42 +0000

    add example test case for populate config with imported routes

--- routeimporter/routeimporter_test.go
+++ routeimporter/routeimporter_test.go
@@ -10,7 +10,64 @@ import (
 	"github.com/open-traffic-generator/snappi/gosnappi"
 )
 
-func TestImportRoutesSmall(t *testing.T) {
+func TestImportRoutesPopulateConfig(t *testing.T) {
+	api := gosnappi.NewApi()
+
+	config := api.NewConfig()
+
+	dta := config.Devices().Add().SetName("devA")
+
+	txPeer := dta.Bgp().SetRouterId("1.1.1.1").Ipv4Interfaces().Add().SetIpv4Name("intA").Peers().Add().SetName("peerA")
+
+	txPeer.SetPeerAddress("1.1.1.1").SetAsType(gosnappi.BgpV4PeerAsType.EBGP)
+	txgroup := txPeer.V4RouteGroups().Add().SetName("groupTxV4")
+
+	if js, err := config.ToJson(); err == nil {
+		fmt.Printf("Config Before Import: %v", js)
+	} else {
+		t.Errorf("failed to convert config in Json format. Error: %v", err.Error())
+	}
+
+	filename := "resource/cisco_v4_basic.txt"
+	fb, err := os.ReadFile(filename)
+	if err != nil {
+		t.Errorf(fmt.Sprintf("Could not Read import file: %s. Error: %v", filename, err))
+		return
+	}
+
+	ic := routeimporter.ImportConfig{
+		SessionName:   "txImp",
+		RRType:        routeimporter.RouteTypeIpv4,
+		RetainNexthop: true,
+		BestRoutes:    true,
+		Targetv4Peers: []gosnappi.BgpV4Peer{txPeer},
+		Targetv6Peers: []gosnappi.BgpV6Peer{},
+	}
+
+	is, err := routeimporter.GetImporterService(routeimporter.ImportFileTypeCisco)
+	if err != nil {
+		t.Errorf(fmt.Sprintf("Could not create Route Importer Service. Error: %v", err))
+	}
+	names, err := is.ImportRoutes(ic, &fb)
+	if err != nil {
+		t.Errorf(fmt.Sprintf("Could not import routes. error: %v", err))
+	}
+	txgroup.SetRouteNames(*names)
+
+	// validate route count
+	expRouteCount := 3
+	if len(txPeer.V4Routes().Items()) != expRouteCount {
+		t.Errorf("Could not successfully imported all routes. Expected Route Count: %d, Imported Routes Count: %d",
+			expRouteCount, len(txPeer.V4Routes().Items()))
+	}
+	if js, err := config.ToJson(); err == nil {
+		fmt.Printf("Config After Import: %v", js)
+	} else {
+		t.Errorf("failed to convert config in Json format. Error: %v", err.Error())
+	}
+}
+
+func TestImportRoutesSimpleV4(t *testing.T) {
 	filename := "resource/cisco_v4_basic.txt"
 
 	is, err := routeimporter.GetImporterService(routeimporter.ImportFileTypeCisco)
@@ -89,7 +146,7 @@ func TestImportRoutesSmall(t *testing.T) {
 	//t.Logf("\n***** importer: %v", is)
 }
 
-func TestImportRoutes1M(t *testing.T) {
+func TestImportRoutes1MV4(t *testing.T) {
 	fmt.Printf("Number of cores: %d\n", runtime.NumCPU())
 
 	is, err := routeimporter.GetImporterService(routeimporter.ImportFileTypeCisco)
